b69161fa849b68e34123014a701ab1de8146a1f6
Francisco Reyes <francisco.reyesja@sansano.usm.cl>
2020-06-30 00:05:11 -0400

    se agrega funcion para ver el tiempo de ejecución

--- Tarea_3_p1/tarea.java
+++ Tarea_3_p1/tarea.java
@@ -9,8 +9,12 @@ public class tarea {
         Scanner numero = new Scanner(System.in);
         int n = numero.nextInt();
         numero.close();
+        final long startTime = System.nanoTime();
         int valor = Funcion.encontrar_numero(n);
+        final long endTime = System.nanoTime();
+		long totalTime = endTime - startTime;
         System.out.println(valor);
+        System.out.println("El tiempo que toma en ejecutarse el programa en segundos es de: " + totalTime/1000000000f + " segundos.");
 
     }
 
