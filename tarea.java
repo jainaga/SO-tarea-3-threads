3054a35a5058c0aba5d80ebb9c298c8d93ab8680
Francisco Reyes <francisco.reyesja@sansano.usm.cl>
2020-06-29 21:44:52 -0400

    camcio de nombre, se ingresa el N

---
+++ tarea.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Tarea {
+    public static void main(String [] args) {
+        System.out.println("Ingrese el número para fijar el rango de búsqueda\n");
+        Scanner numero = new Scanner(System.in);
+        int n = numero.nextInt();
+        numero.close();
+
+    }
+
+
+}
